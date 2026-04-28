12b50fef13b6824f2671b0d40341c4bcdca3de10
aisystem1488 <aisystem1488@gmail.com>
2026-04-28 18:47:42 +0200

    add smooth scroll navigation

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log("Jaffar.hu landing page loaded");
+
+document.querySelectorAll(".hero-actions a[href^='#']").forEach(function (link) {
+  link.addEventListener("click", function (event) {
+    var target = document.querySelector(link.getAttribute("href"));
+    if (target) {
+      event.preventDefault();
+      target.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
